1e373b8ef62ba7448d66be80d5ec35167f6eb5a5
Aniruddha Basak <codewithaniruddha@gmail.com>
2022-04-03 20:44:16 +0530

    Add save method to resize image while saving
    Signed-off-by: Aniruddha Basak <codewithaniruddha@gmail.com>

--- api/models.py
+++ api/models.py
@@ -1,9 +1,11 @@
+import imp
 import django
 from django.db import models
 from datetime import datetime
+from api.resize_mixings import ResizeImageMixin
 
 
-class Record(models.Model):
+class Record(models.Model, ResizeImageMixin):
     name = models.CharField(max_length=50)
     species = models.CharField(max_length=50)
     weight = models.PositiveIntegerField()
@@ -11,3 +13,12 @@ class Record(models.Model):
     log = models.FloatField()
     timestamp = models.DateTimeField(default=django.utils.timezone.now)
     photo = models.ImageField(upload_to="media/images", default="")
+
+    def save(self, *args, **kwargs):
+        if self.pk is None:
+            self.resize(self.photo, (140, 140))
+
+        super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return "{} - {} - {}".format(self.name, self.species, self.timestamp)
